317d371697b73d948b89ae3dce69365dc3648d9e
Devin Gaffney <itsme@devingaffney.com>
2017-09-20 13:17:12 -0400

    even more.

--- dataminer/extractor.py
+++ dataminer/extractor.py
@@ -6,53 +6,53 @@ import json
 import os
 from datetime import datetime
 class Extractor:
-  def current_user():
+  def current_user(self):
     return os.popen("whoami").read().split("\n")[0]
   
   def kickoff(self, hashtag_set, hashtag_operator, start_time, end_time, data_fullness=None, working_directory=None):
     start_time = datetime.strptime(start_time, '%Y-%m-%d')
     end_time = datetime.strptime(end_time, '%Y-%m-%d')
-    if end_time-start_time < 0:
+    if (end_time-start_time).days < 0:
       return "I can't go backwards in time!"
     if data_fullness == None:
       data_fullness="reduced"
     if working_directory == None:
-      working_directory="/home/"+current_user()
+      working_directory="/home/"+self.current_user()
     if hashtag_operator == "AND":
       print "Extracting "+str.join(",", hashtag_set)
-      extract(hashtag_set, start_time, end_time, data_fullness, working_directory)
+      self.extract(hashtag_set, start_time, end_time, data_fullness, working_directory)
     elif hashtag_operator == "OR":
       for hashtag in hashtag_set:
         print "Extracting "+hashtag
-        extract([hashtag], start_time, end_time, data_fullness, working_directory)
+        self.extract([hashtag], start_time, end_time, data_fullness, working_directory)
     else:
       return "Hashtag Operator MUST BE AND or OR, you goof."
   
   def extract(self, hashtag_set, start_time, end_time, data_fullness, working_directory):
     files = []
     if data_fullness == "reduced":
-      files = restricted_to_timeline(ls(reduced_data_path()), start_time, end_time)
+      files = self.restricted_to_timeline(self.ls(self.reduced_data_path()), start_time, end_time)
     else:
-      files = restricted_to_timeline(ls(full_data_path()), start_time, end_time)
-    create_corpus(hashtag_set, start_time, end_time, data_fullness, working_directory)
+      files = self.restricted_to_timeline(self.ls(self.full_data_path()), start_time, end_time)
+    self.create_corpus(hashtag_set, start_time, end_time, data_fullness, working_directory)
     for file in files:
-      extract_file(file, hashtag_set, data_fullness)
+      self.extract_file(file, hashtag_set, data_fullness)
   
   def fullpath(self, working_directory, hashtag_set, start_time, end_time, data_fullness):
-    return working_directory+"/hashtag_extractions/"+corpus_name(hashtag_set, start_time, end_time, data_fullness)+"/"
+    return working_directory+"/hashtag_extractions/"+self.corpus_name(hashtag_set, start_time, end_time, data_fullness)+"/"
 
   def extract_file(self, file, hashtag_set, data_fullness, working_directory):
     if data_fullness == "reduced":
-      os.popen("lz4 -dc "+reduced_data_path()+"/"+file+" | awk '/"+str.join("/ && /", hashtag_set)+"/' > "+fullpath(working_directory, hashtag_set, start_time, end_time, data_fullness)+str.replace(file, ".lz4", ".csv"))
+      os.popen("lz4 -dc "+self.reduced_data_path()+"/"+file+" | awk '/"+str.join("/ && /", hashtag_set)+"/' > "+self.fullpath(working_directory, hashtag_set, start_time, end_time, data_fullness)+str.replace(file, ".lz4", ".csv"))
       #todo awk does not capture totally unique hashtags but instead captures substrings of hashtags - eg. searching for #ff will also extract #ffvi #ffix and etc
     else:
-      os.popen("xzcat "+full_data_path()+"/"+file+" | awk '/"+str.join("/ && /", hashtag_set)+"/' > "+fullpath(working_directory, hashtag_set, start_time, end_time, data_fullness)+str.replace(file, ".xz", ".csv"))
+      os.popen("xzcat "+self.full_data_path()+"/"+file+" | awk '/"+str.join("/ && /", hashtag_set)+"/' > "+self.fullpath(working_directory, hashtag_set, start_time, end_time, data_fullness)+str.replace(file, ".xz", ".csv"))
   
   def corpus_name(self, hashtag_set, start_time, end_time, data_fullness):
     return str.join("_", hashtag_set)+"_"+start_time.strftime("%Y-%m-%d")+"_"+end_time.strftime("%Y-%m-%d")+"_"+data_fullness
   
   def create_corpus(self, hashtag_set, start_time, end_time, data_fullness):
-    os.popen("mkdir -p "+working_directory+"/hashtag_extractions/"+corpus_name(hashtag_set, start_time, end_time, data_fullness))
+    os.popen("mkdir -p "+working_directory+"/hashtag_extractions/"+self.corpus_name(hashtag_set, start_time, end_time, data_fullness))
     #todo also write a flat file at this point specifying wtf it is that we've requested off the servers.
   
   def restricted_to_timeline(self, files, start_time, end_time):
