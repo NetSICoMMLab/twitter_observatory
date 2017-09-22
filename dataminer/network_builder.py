ae57191110b9b440bd3b5d09a903251d5f889aa5
Ryan J. Gallagher <ryanjgallag@gmail.com>
2017-09-22 15:51:46 -0400

    Initialize network_builder.py

---
+++ dataminer/network_builder.py
@@ -0,0 +1,12 @@
+"""Network Builder (network.py)
+Takes a directory of tweets, builds a network, and returns relevant statistics
+about the network
+
+Contributors:
+Devin Gaffney & Ryan J. Gallagher
+Network Science Institute, Northeastern University, 2017
+"""
+import os
+import csv
+import json
+import networkx as nx
